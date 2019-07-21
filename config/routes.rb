88de083233c0da46ed15da2f4ca43e4ec046c756
AAlrifae <a.a.alrifae@gmail.com>
2019-07-21 14:28:59 +0300

    create exercise database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :exercises
   resources :recipes
   resources :days
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_21_111450) do
+ActiveRecord::Schema.define(version: 2019_07_21_112811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2019_07_21_111450) do
 
   create_table "day", force: :cascade do |t|
     t.date "day"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "exercises", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
